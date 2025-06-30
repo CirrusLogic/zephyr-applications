c910c28eecd8e3a79f37007a4aa8238bd793d086
Ricardo Rivera-Matos <rriveram@opensource.cirrus.com>
2025-06-30 14:23:15 -0500

    cirrus: cs35l56: Adds support for mute/unmute via SW0
    Uses the SW0 button to toggle the mute status.
    
    Signed-off-by: Ricardo Rivera-Matos <rriveram@opensource.cirrus.com>

--- cirrus/cs35l56/src/main.c
+++ cirrus/cs35l56/src/main.c
@@ -9,6 +9,69 @@
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/audio/codec.h>
+#include <zephyr/drivers/gpio.h>
+
+#ifdef CONFIG_TOGGLE_AMP_MUTE_SW0
+static volatile bool mute_enabled = false;
+#define SW0_NODE        DT_ALIAS(sw0)
+#endif
+
+#ifdef CONFIG_TOGGLE_AMP_MUTE_SW0
+static struct gpio_dt_spec sw0_spec = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+#endif
+
+#ifdef CONFIG_TOGGLE_AMP_MUTE_SW0
+static void sw0_handler(const struct device *dev, struct gpio_callback *cb,
+			uint32_t pins)
+{
+	audio_property_value_t val;
+	int ret;
+
+	val.mute = !mute_enabled;
+	ret = audio_codec_set_property(dev, AUDIO_PROPERTY_INPUT_MUTE, AUDIO_CHANNEL_FRONT_LEFT, val);
+	if (ret < 0) {
+		printk("\nError: Failed to toggle mute\n");
+		return;
+	}
+
+	mute_enabled = val.mute;
+	printk("Mute %sabled\n", (mute_enabled ? "en" : "dis"));
+}
+#endif
+
+#ifdef CONFIG_TOGGLE_AMP_MUTE_SW0
+static bool init_button(void)
+{
+	static struct gpio_callback sw0_cb_data;
+	int ret;
+
+	if (!gpio_is_ready_dt(&sw0_spec)) {
+		printk("%s is not ready\n", sw0_spec.port->name);
+		return false;
+	}
+
+	ret = gpio_pin_configure_dt(&sw0_spec, GPIO_INPUT);
+	if (ret < 0) {
+		printk("Failed to configure %s pin %d: %d\n",
+		       sw0_spec.port->name, sw0_spec.pin, ret);
+		return false;
+	}
+
+	ret = gpio_pin_interrupt_configure_dt(&sw0_spec,
+					      GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret < 0) {
+		printk("Failed to configure interrupt on %s pin %d: %d\n",
+		       sw0_spec.port->name, sw0_spec.pin, ret);
+		return false;
+	}
+
+	gpio_init_callback(&sw0_cb_data, sw0_handler, BIT(sw0_spec.pin));
+	gpio_add_callback(sw0_spec.port, &sw0_cb_data);
+	printk("Press \"%s\" to toggle the amp mute\n", sw0_spec.port->name);
+
+	return true;
+}
+#endif
 
 #define CODEC_DAI_CFG_I2S_STEREO	2
 
@@ -66,6 +129,12 @@ int main(void)
 		printk("\nError: Failed to unmute\n");
 		return ret;
 	}
+
+#ifdef CONFIG_TOGGLE_AMP_MUTE_SW0
+	if (!init_button()) {
+		return 0;
+	}
+#endif
 
 	audio_codec_start_output(dev);
 
