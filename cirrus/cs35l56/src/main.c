c8b89118a8c2a9886a240e3f271bd39cff6b0e03
Ricardo Rivera-Matos <rriveram@opensource.cirrus.com>
2025-08-06 17:49:06 -0500

    cirrus: cs35l56: src: Introduces major improvements
    Introduces major improvements, most notably support for both stereo
    and quad amp setups.
    
    Signed-off-by: Ricardo Rivera-Matos <rriveram@opensource.cirrus.com>

--- cirrus/cs35l56/src/main.c
+++ cirrus/cs35l56/src/main.c
@@ -9,6 +9,45 @@
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/audio/codec.h>
+#include <zephyr/drivers/gpio.h>
+
+#define AUDIO_AMP_LEFT			0
+#define AUDIO_AMP_RIGHT			1
+#define AUDIO_AMP_BACK_LEFT		2
+#define AUDIO_AMP_BACK_RIGHT	3
+
+#define ASP1_RX1	1
+#define ASP1_RX2	2
+
+const struct device *amps[] = {
+	[AUDIO_AMP_LEFT] = DEVICE_DT_GET(DT_NODELABEL(cs35l56_l)),
+	[AUDIO_AMP_RIGHT] = DEVICE_DT_GET(DT_NODELABEL(cs35l56_r)),
+	[AUDIO_AMP_BACK_LEFT] = DEVICE_DT_GET_OR_NULL(DT_NODELABEL(cs35l56_bl)),
+	[AUDIO_AMP_BACK_RIGHT] = DEVICE_DT_GET_OR_NULL(DT_NODELABEL(cs35l56_br)),
+};
+
+static volatile bool toggle_playback = false;
+
+#define SW0_NODE	DT_ALIAS(sw0)
+#if !DT_NODE_HAS_STATUS_OKAY(SW0_NODE)
+#error "Unsupported board: sw0 devicetree alias is not defined"
+#endif
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
+							      {0});
+static struct gpio_callback button_cb_data;
+
+/*
+ * The led0 devicetree alias is optional. If present, we'll use it
+ * to turn on the LED whenever the button is pressed.
+ */
+static struct gpio_dt_spec led = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios,
+						     {0});
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb,
+		    uint32_t pins)
+{
+	toggle_playback = true;
+}
 
 #define CODEC_DAI_CFG_I2S_STEREO	2
 
@@ -20,7 +59,7 @@
 									 I2S_OPT_FRAME_CLK_SLAVE)
 
 struct audio_codec_cfg cfg = {
-	.dai_cfg.i2s.word_size = AUDIO_PCM_WIDTH_32_BITS,
+	.dai_cfg.i2s.word_size = AUDIO_PCM_WIDTH_24_BITS,
 	.dai_cfg.i2s.channels = CODEC_DAI_CFG_I2S_STEREO,
 	.dai_cfg.i2s.frame_clk_freq = AUDIO_PCM_RATE_48K,
 	.dai_cfg.i2s.format = CODEC_DAI_CFG_I2S_FORMAT,
@@ -30,76 +69,169 @@ struct audio_codec_cfg cfg = {
 	.dai_type = AUDIO_DAI_TYPE_I2S,
 };
 
-int main(void)
+static int init_codecs(void)
 {
-	const struct device *amp_l = DEVICE_DT_GET(DT_NODELABEL(cs35l56_l));
-	const struct device *amp_r = DEVICE_DT_GET(DT_NODELABEL(cs35l56_r));
 	audio_property_value_t val;
 	int ret;
 
-	if (amp_l == NULL || amp_r == NULL ) {
+	if (amps[AUDIO_AMP_LEFT] == NULL || amps[AUDIO_AMP_RIGHT] == NULL ) {
 		printk("No CS35L56 stereo pair found...\n");
 		return -ENODEV;
 	}
 
-	if (!device_is_ready(amp_l)) {
-		printk("\nError: Left \"%s\" is not ready; "
-			"check the driver initialization logs for errors.\n",
-			amp_l->name);
-			return -EIO;
-	}
-
-	if (!device_is_ready(amp_r)) {
-		printk("\nError: Right \"%s\" is not ready; "
-			"check the driver initialization logs for errors.\n",
-			amp_r->name);
-			return -EIO;
-	}
-
-	ret = audio_codec_configure(amp_l, &cfg);
-	if (ret < 0) {
-		printk("\nError: Failed to configure left codec.\n");
-		return ret;
-	}
-
-	ret = audio_codec_configure(amp_r, &cfg);
-	if (ret < 0) {
-		printk("\nError: Failed to configure right codec.\n");
-		return ret;
-	}
-
-	ret = audio_codec_route_input(amp_l, AUDIO_CHANNEL_FRONT_LEFT, 1);
-	if (ret < 0) {
-		printk("\nError: Failed to route left input path.\n");
-		return ret;
-	}
-
-	ret = audio_codec_route_input(amp_r, AUDIO_CHANNEL_FRONT_RIGHT, 2);
-	if (ret < 0) {
-		printk("\nError: Failed to route right input path.\n");
-		return ret;
-	}
-
 	val.mute = false;
 
-	ret = audio_codec_set_property(amp_l, AUDIO_PROPERTY_INPUT_MUTE, AUDIO_CHANNEL_FRONT_LEFT, val);
-	if (ret < 0) {
-		printk("\nError: Failed to unmute left amp\n");
+	for (int i = 0; i < ARRAY_SIZE(amps); i++) {
+		if (amps[i] == NULL) {
+			continue;
+		}
+
+		if (!device_is_ready(amps[i])) {
+			printk("\n Error: %s is not ready;\n", amps[i]->name);
+			return -EIO;
+		}
+
+		ret = audio_codec_configure(amps[i], &cfg);
+		if (ret < 0) {
+			printk("\nError: Failed to configure %s codec.\n", amps[i]->name);
+			return ret;
+		}
+
+		if ((i % 2) == 0) {
+			ret = audio_codec_route_input(amps[i], AUDIO_CHANNEL_FRONT_LEFT, ASP1_RX1);
+			if (ret < 0) {
+				printk("\nError: Failed to route left input path.\n");
+				return ret;
+			}
+
+			ret = audio_codec_set_property(amps[i], AUDIO_PROPERTY_INPUT_MUTE, AUDIO_CHANNEL_FRONT_LEFT, val);
+			if (ret < 0) {
+				printk("\nError: Failed to unmute left amp, %d\n", ret);
+				return ret;
+			}
+		} else {
+			ret = audio_codec_route_input(amps[i], AUDIO_CHANNEL_FRONT_RIGHT, ASP1_RX2);
+			if (ret < 0) {
+				printk("\nError: Failed to route right input path.\n");
+				return ret;
+			}
+
+			ret = audio_codec_set_property(amps[i], AUDIO_PROPERTY_INPUT_MUTE, AUDIO_CHANNEL_FRONT_RIGHT, val);
+			if (ret < 0) {
+				printk("\nError: Failed to unmute right amp\n");
+				return ret;
+			}
+		}
+	}
+
+	return 0;
+}
+
+static void set_codec_output_state(const bool playback)
+{
+	printk("%s audio playback\n", playback ? "Starting" : "Pausing");
+	if (playback) {
+		init_codecs();
+		for (int i = 0; i < ARRAY_SIZE(amps); i++) {
+			if (amps[i] == NULL) {
+				continue;
+			}
+			audio_codec_start_output(amps[i]);
+		}
+	} else {
+		for (int i = 0; i < ARRAY_SIZE(amps); i++) {
+			if (amps[i] == NULL) {
+				continue;
+			}
+			audio_codec_stop_output(amps[i]);
+		}
+	}
+	gpio_pin_set_dt(&led, playback);
+}
+
+static void init_led(void)
+{
+	int ret = 0;
+
+	if (led.port && !gpio_is_ready_dt(&led)) {
+		printk("Error %d: LED device %s is not ready; ignoring it\n",
+		       ret, led.port->name);
+		led.port = NULL;
+	}
+	if (led.port) {
+		ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT);
+		if (ret != 0) {
+			printk("Error %d: failed to configure LED device %s pin %d\n",
+			       ret, led.port->name, led.pin);
+			led.port = NULL;
+		} else {
+			printk("Set up LED at %s pin %d\n", led.port->name, led.pin);
+		}
+	}
+}
+
+static int init_button(void)
+{
+	int ret;
+
+	if (!gpio_is_ready_dt(&button)) {
+		printk("Error: button device %s is not ready\n",
+		       button.port->name);
+		return -ENODEV;
+	}
+
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret != 0) {
+		printk("Error %d: failed to configure %s pin %d\n",
+		       ret, button.port->name, button.pin);
 		return ret;
 	}
 
-	ret = audio_codec_set_property(amp_r, AUDIO_PROPERTY_INPUT_MUTE, AUDIO_CHANNEL_FRONT_RIGHT, val);
-	if (ret < 0) {
-		printk("\nError: Failed to unmute right amp\n");
+	ret = gpio_pin_interrupt_configure_dt(&button,
+					      GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret != 0) {
+		printk("Error %d: failed to configure interrupt on %s pin %d\n",
+			ret, button.port->name, button.pin);
 		return ret;
 	}
 
-	audio_codec_start_output(amp_l);
-	audio_codec_start_output(amp_r);
+	return 0;
+}
+
+int main(void)
+{
+	bool playback = false;
+	int ret;
+
+	ret = init_button();
+	if (ret != 0) {
+		printk("Failed to initialize button\n");
+	}
+
+	init_led();
+
+	ret = init_codecs();
+	if (ret != 0) {
+		printk("Failed to initialize codecs\n");
+		return ret;
+	}
+
+	set_codec_output_state(playback);
+
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb_data);
+	printk("Set up button at %s pin %d\n", button.port->name, button.pin);
 
 	while (1) {
-		printk("Audio playback in progress\n");
-		k_sleep(K_SECONDS(10));
+		if (toggle_playback) {
+			playback = !playback;
+			set_codec_output_state(playback);
+			toggle_playback = false;
+		}
+
+		printk("Audio playback %s\n", playback ? "in progress" : "is paused");
+
+		k_sleep(K_SECONDS(1));
 	}
 
 	return 0;
